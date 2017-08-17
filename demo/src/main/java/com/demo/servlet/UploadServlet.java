f138aa55608058f22e02bb4910f22b2c95af06f5
YuKimCai <chaozhouhy@gmail.com>
2017-08-17 16:12:38 +0800

    T哦身体

---
+++ demo/src/main/java/com/demo/servlet/UploadServlet.java
@@ -0,0 +1,89 @@
+package com.demo.servlet;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class UploadServlet extends javax.servlet.http.HttpServlet {
+    //上传文件储存目录
+    private static final String UPLOAD_DIRECTORY = "upload";
+    //上传配置
+    private static final int MEMORY_THRESHOLD = 1024 * 1024 * 3;
+    private static final int MAX_FILE_SIZE = 1024 * 1024 * 40;
+    private static final int MAX_REQUEST_SIZE = 1024 * 1024 * 50;
+    /**
+     * 上传数据及文件
+     */
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+        //检测是否为多媒体上传
+        if (!ServletFileUpload.isMultipartContent(request)) {
+            //如果不是则停止
+            PrintWriter writer = response.getWriter();
+            writer.println("Error：表单必须包含 enctype = multipart/form-data");
+            writer.flush();
+            return;
+        }
+        //配置上传参数
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+        //设置内存临界值-超过后将产生临时文件并储存于临时目录中
+        factory.setSizeThreshold(MEMORY_THRESHOLD);
+        //设置临时存储目录
+        factory.setRepository(new File(System.getProperty("java.io.tmpdir")));
+
+        ServletFileUpload upload = new ServletFileUpload(factory);
+
+        //设置最大文件上传值
+        upload.setFileSizeMax(MAX_FILE_SIZE);
+
+        //设置最大请求值
+        upload.setSizeMax(MAX_REQUEST_SIZE);
+
+        //中文处理
+        upload.setHeaderEncoding("UTF-8");
+
+        //构造临时路径存储上传文件
+        //这个路径相对当前应用的目录
+        String uploadPath = request.getServletContext().getRealPath("../") + File.separator + UPLOAD_DIRECTORY;
+
+        //如果目录不存在则创建
+        File uplocadDir = new File(uploadPath);
+        if (!uplocadDir.exists()) {
+            uplocadDir.mkdir();
+        }
+
+        try {
+            @SuppressWarnings("unchecked")
+            List<FileItem> formItems = upload.parseRequest(request);
+
+            if (formItems != null && formItems.size() > 0 ) {
+                //迭代表单数据
+                for (FileItem item : formItems) {
+                    //处理不在表单中的字段
+                    if (!item.isFormField()) {
+                        String fileName = new File(item.getName()).getName();
+                        String filePath = uploadPath + File.separator + fileName;
+                        File storeFile = new File(filePath);
+                        //在控制台输出文件的上传路径
+                        System.out.println(filePath);
+                        //保存文件到硬盘
+                        item.write(storeFile);
+                        request.setAttribute("message","文件上传成功");
+                    }
+                }
+            }
+        } catch (Exception e) {
+            request.setAttribute("message","错误信息：" + e.getMessage());
+        }
+        request.getServletContext().getRequestDispatcher("/message.jsp").forward(request,response);
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
